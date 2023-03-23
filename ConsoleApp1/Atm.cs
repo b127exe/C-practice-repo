c0292e115b866f637d9e934cd014788c4ae9f387
b127exe <bilalimran6217523@gmail.com>
2023-03-23 18:14:37 +0500

    ATM working and practice on methods

---
+++ ConsoleApp1/Atm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Atm
+    {
+        static void Main(string[] args)
+        {
+
+            int balance = 10000;
+            int reservePin = 123;
+            int count = 0;
+
+            pinStart:
+            Console.Clear();
+
+            Console.WriteLine("\t\t\t\t\t========================================");
+            Console.WriteLine("\t\t\t\t\t\t" + @"   /\    ------- |\    /|");
+            Console.WriteLine("\t\t\t\t\t\t" + @"  /  \      |    | \  / |");
+            Console.WriteLine("\t\t\t\t\t\t" + @" /----\     |    |  \/  |");
+            Console.WriteLine("\t\t\t\t\t\t" + @"/      \    |    |      |");
+            Console.WriteLine("\t\t\t\t\t========================================");
+
+            Console.Write("\n\t\t\t\t\tENTER PIN: ");
+            int enterPin = int.Parse(Console.ReadLine());
+
+            if( enterPin == reservePin )
+            {
+                Console.WriteLine("\n\t\t\t\t\t1) Balance Inquiry \t\t2) Fast Cash \n\n\t\t\t\t\t3) Cash WithDrawel \t\t4) PIN Change \n\n\t\t\t\t\t5) Exit");
+
+                Console.Write("\n\t\t\t\t\tEnter Option: ");
+                int option = int.Parse(Console.ReadLine());
+
+                if(option == 1)
+                {
+                    Console.WriteLine("\n\t\t\t\t\t========================================");
+                    Console.WriteLine("\n\t\t\t\t\t\tBALANCE INQUIRY");
+                    Console.WriteLine("\n\t\t\t\t\tAcc no:1348446 \n\n\t\t\t\t\tName: Bilal Khan \n\n\t\t\t\t\tTotal Balance: " + balance + "Rs");
+                }
+                else if (option == 2)
+                {
+                    Console.WriteLine("\n\t\t\t\t\t========================================");
+                    Console.WriteLine("\n\t\t\t\t\t\tFAST CASH OPTIONS \n\n\t\t\t\t\t1) 500 \t\t\t\t2) 1000 \n\n\t\t\t\t\t3) 2000 \t\t\t4) 5000");
+                    Console.Write("\n\t\t\t\t\tEnter Option: ");
+                    int fastOp = int.Parse(Console.ReadLine());
+                    if(fastOp <= balance)
+                    {
+                    if(fastOp == 1)
+                    {
+                        balance = balance - 500;
+                            Console.WriteLine("\n\t\t\t\t\t========================================");
+                            Console.WriteLine("\n\t\t\t\t\tFast Cash Successfull \n\n\t\t\t\t\tYour current balance is " + balance + "Rs");
+                    }
+                    else if(fastOp == 2) {
+                            balance = balance - 1000;
+                            Console.WriteLine("\n\t\t\t\t\t========================================");
+                            Console.WriteLine("\n\t\t\t\t\tFast Cash Successfull \n\n\t\t\t\t\tYour current balance is " + balance + "Rs");
+                        }
+                    else if(fastOp == 3)
+                        {
+                            balance = balance - 2000;
+                            Console.WriteLine("\n\t\t\t\t\t========================================");
+                            Console.WriteLine("\n\t\t\t\t\tFast Cash Successfull \n\n\t\t\t\t\tYour current balance is " + balance + "Rs");
+                        }
+                    else if(fastOp == 4)
+                        {
+                            balance = balance - 5000;
+                            Console.WriteLine("\n\t\t\t\t\t========================================");
+                            Console.WriteLine("\n\t\t\t\t\tFast Cash Successfull \n\n\t\t\t\t\tYour current balance is " + balance + "Rs");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\n\t\t\t\t\tInvalid Option");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n\t\t\t\t\tInsufficient Balance...");
+                    }
+                }
+                else if(option == 3)
+                {
+                    Console.WriteLine("\n\t\t\t\t\t========================================");
+                    Console.WriteLine("\n\t\t\t\t\t\tCASH WITHDRAWEL");
+
+                    Console.Write("\n\t\t\t\t\tEnter Withdawel amount: ");
+                    int withAmount = int.Parse(Console.ReadLine());
+
+                    if(withAmount <= balance)
+                    {
+                        balance = withAmount - balance;
+                        Console.WriteLine("\n\t\t\t\t\t========================================");
+                        Console.WriteLine("\n\t\t\t\t\t{0}Rs Withdraw successfully! \n\n\t\t\t\t\tYour current balance is {1}Rs",withAmount,balance);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n\t\t\t\t\tThe amout you enter is insufficient...");
+                    }
+                }
+                else if(option == 4)
+                {
+                changePIN:
+                    Console.Clear();
+                    Console.WriteLine("\n\t\t\t\t\t========================================");
+                    Console.WriteLine("\n\t\t\t\t\t\tPIN CHANGE");
+
+                    Console.Write("\n\t\t\t\t\tEnter Old PIN: ");
+                    int oldPIN = int.Parse(Console.ReadLine());
+
+                    if(oldPIN == reservePin)
+                    {
+                        Console.Write("\n\t\t\t\t\tEnter New PIN: ");
+                        int newPIN = int.Parse(Console.ReadLine());
+
+                        reservePin = newPIN;
+                        Console.WriteLine("\n\t\t\t\t\t========================================");
+                        Console.WriteLine("\n\t\t\t\t\tPIN successfully changed...\n\n\t\t\t\t\tYou are about logout in 5 seconds.");
+                        Thread.Sleep(5000);
+                        goto pinStart;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n\t\t\t\t\tOld PIN not match...");
+                        Thread.Sleep(2000);
+                        goto changePIN;
+                    }
+                }
+                else if(option == 5)
+                {
+                    goto end;
+                }
+                else
+                {
+                    Console.WriteLine("\n\t\t\t\t\tThe option you enter is unavailable...");
+
+                    Console.Write("\n\t\t\t\t\tDo you want to continue... ( yes / no )");
+                    string op = Console.ReadLine();
+                    if(op == "yes")
+                    {
+                        goto pinStart;
+                    }
+                    else
+                    {
+                        goto end;
+                    }
+                }
+
+            }
+            else
+            {
+                count++;
+                if(count == 3)
+                {
+                    goto end;
+                }
+                else
+                {
+                Console.WriteLine("\n\t\t\t\t\tIncorrect Pin");
+                    Thread.Sleep(2000);
+                goto pinStart;
+                }
+            }
+
+            end:
+            Console.WriteLine("\n\t\t\t\t\tPROGRAM END...");
+
+
+            Console.ReadKey();
+        }
+    }
+}
--- ConsoleApp1/practice.cs
+++ ConsoleApp1/practice.cs
@@ -1,16 +1,192 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class practice
     {
-        static void Main(string[] args)
+
+        /* Function Types
+         * 1) with argument and no return
+         * 2) no argument and with return
+         * 3) no  argument and no return
+         * 4) with argument and with return
+         */
+        
+        // no argument and no return
+        public void myFunc1()
+        {
+            Console.WriteLine("This is function with no argument and no return");
+        }
+
+        // with argument and with return
+        public int myFunc2(int a, int b)
+        {
+            return a + b;
+        }
+
+        // with argument and no return
+        public void myFunc3(string name)
+        {
+            Console.WriteLine("Mr. " + name);
+        }
+
+        // no argument and with return
+        public int myFunc4()
+        {
+            int i = 10;
+            int j = 14;
+            
+            return i + j;
+        }
+
+        // measure grade method
+
+        public char takeGrade(int per)
+        {
+            char grade;
+
+            if(per >= 90 && per <= 100)
+            {
+                grade = 'A';
+            }
+            else if(per >= 80 && per <= 90)
+            {
+                grade = 'B';
+            }
+            else if(per >= 70 && per <= 80)
+            {
+                grade = 'C';
+            }
+            else if(per >= 50 && per <= 70)
+            {
+                grade = 'D';
+            }
+            else
+            {
+                grade = 'F';
+            }
+
+            return grade;
+        }
+
+        static void sample()
         {
             Console.WriteLine("Hello World");
+        }
+
+        static void Main(string[] args)
+        {
+            //sample();
+
+            //int math = 80;
+            //int english = 60;
+            //int urdu = 100;
+
+            //int obt = math + english + urdu;
+            //int per = (obt * 100) / 300;
+
+            //var bilal = new practice();
+            //Console.WriteLine("Your grade is "+bilal.takeGrade(per));
+
+            // 4 types of methods 
+
+            //    var p4 = new practice();
+            //    Console.WriteLine(p4.myFunc4());
+
+            //var p3 =  new practice();
+            //p3.myFunc3("Bilal Khan");
+
+            //var p1 = new practice();
+            //p1.myFunc1();
+
+
+            //Console.Write("Enter first value: ");
+            //int f = int.Parse(Console.ReadLine());
+            //Console.Write("Enter second value: ");
+            //int s = int.Parse(Console.ReadLine());
+
+            //var p2 = new practice();
+            //Console.WriteLine(p2.myFunc2(f,s));
+
+
+            //Array Practice
+
+            //int[] array = new int[4] {
+            // 45,
+            // 67,
+            // 32,
+            // 12
+            //};
+
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.WriteLine(array[i]);
+            //}
+
+            //Console.Write("Enter length: ");
+            //int len = Convert.ToInt32(Console.ReadLine());
+
+            //int[] nums = new int[len];
+
+            //for(int i = 0; i < len; i++)
+            //{
+            //    Console.Write("Enter Array Values {0}: ",i+1);
+            //    nums[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("===============================");
+
+            //foreach (int i  in nums)
+            //{
+
+            //    Console.WriteLine(i);
+            //}
+
+            //string[,] names = new string[3, 2];
+            //names[0, 0] = "Bilal";
+            //names[0, 1] = "Ali";
+            //names[1, 0] = "Sara";
+            //names[1, 1] = "Ayesha";
+            //names[2, 0] = "Haris";
+            //names[2, 1] = "Zain";
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        Console.WriteLine(names[i, j]);
+            //    }
+            //}
+
+            //int[] num = {12, 23, 56, 17};
+
+            //foreach (int i in num)
+            //{
+            //   Console.WriteLine(i);
+            //}
+
+            //int pin;
+
+            //do
+            //{
+            //    Console.Write("Enter Pin: ");
+            //    pin = int.Parse(Console.ReadLine());
+            //} while (pin != 123);
+
+            //Console.WriteLine("Pin Successfully connect!");
+
+            //Console.WriteLine(@"   /\    ======= |\    /|");
+            //Console.WriteLine(@"  /  \      |    | \  / |");
+            //Console.WriteLine(@" /----\     |    |  \/  |");  
+            //Console.WriteLine(@"/      \    |    |      |");
+
 
             Console.ReadKey();  
         }
